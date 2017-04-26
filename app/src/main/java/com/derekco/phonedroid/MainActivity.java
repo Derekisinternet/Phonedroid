41c9f2da0eefdfaa7567d8531c67680d6e7513ad
Derek <derekisinternet@gmail.com>
2017-04-25 17:37:07 -0700

    first commit

---
+++ app/src/main/java/com/derekco/phonedroid/MainActivity.java
@@ -0,0 +1,36 @@
+package com.derekco.phonedroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.derekco.phonedroid.EXAMPLE_MESSAGE";
+    public static final String COMMAND_TYPE = "com.derekco.phonedroid.COMMAND_TYPE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendCommand(View view) {
+        Intent intent = new Intent(this, SendCommandActivity.class);
+        intent.putExtra(COMMAND_TYPE, "stop");
+        startActivity(intent);
+    }
+
+    public void sendMessage(View view) {
+        // do something on button press
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/derekco/phonedroid/SendCommandActivity.java
@@ -0,0 +1,13 @@
+package com.derekco.phonedroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SendCommandActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_command);
+    }
+}
