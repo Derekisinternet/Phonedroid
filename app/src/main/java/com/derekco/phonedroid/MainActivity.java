f972dcf0098a667eefc8705f9eac5d78bb70754e
Derek <derekisinternet@gmail.com>
2017-05-20 18:50:12 -0700

    too much to count

--- app/src/main/java/com/derekco/phonedroid/MainActivity.java
+++ app/src/main/java/com/derekco/phonedroid/MainActivity.java
@@ -5,6 +5,7 @@ import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -43,6 +44,11 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+
+    }
+
+    public void sendForwardCommand(View view) {
+        Log.d("Main", "Forward Button Press!");
 
     }
 }
