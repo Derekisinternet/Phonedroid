7dafe5c1d6367c1370060650fb789f7c99d957be
Derek <derekisinternet@gmail.com>
2017-06-12 22:26:41 -0700

    refactoring

--- app/src/main/java/com/derekco/phonedroid/MainActivity.java
+++ app/src/main/java/com/derekco/phonedroid/MainActivity.java
@@ -1,17 +1,13 @@
 package com.derekco.phonedroid;
 
 
-import android.os.Handler;
-import android.os.Message;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -71,82 +67,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-//    // Create a BroadcastReceiver for ACTION_FOUND.
-//    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-//        public void onReceive(Context context, Intent intent) {
-//            String action = intent.getAction();
-//
-//            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-//                Log.d(TAG, "Discovered device");
-//                // Discovery has found a device. Get the BluetoothDevice
-//                // object and its info from the Intent.
-//                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-//                String deviceName = device.getName();
-//
-//                if (deviceName != null) {
-//                    Log.d(TAG, "Device Name: " + deviceName);
-//                    Log.d(TAG, String.valueOf(deviceName.equals("ALPHA")));
-//                } else {
-//                    System.out.println("Problem getting device name.");
-//                }
-//                // TODO: get rid of hard-code to allow for multiple devices
-//                // right now, we're only looking for ALPHA, so . . .
-//                if (deviceName.equals("ALPHA")) {
-//                    Log.d(TAG, "Starting Bluetooth Connection . . .");
-//                    mBluetoothService.connect(device, true);
-//                    mConnectedDeviceName = deviceName;
-//                }
-//            } else if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
-//                Log.d(TAG, "Starting Bluetooth Device Discovery.");
-//            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-//                Log.d(TAG, "Bluetooth Discovery Finished");
-//            }
-//        }
-//    };
-
-    /**
-     * Handler for communication with the BluetoothService:
-    */
-    private Handler btHandler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case BluetoothConstants.MESSAGE_STATE_CHANGE:
-                    switch (msg.arg1) {
-                        case BluetoothService.STATE_CONNECTED:
-                            break;
-                        case BluetoothService.STATE_CONNECTING:
-//                            setStatus(R.string.title_connecting);
-                            break;
-                        case BluetoothService.STATE_LISTEN:
-                        case BluetoothService.STATE_NONE:
-//                            setStatus(R.string.title_not_connected);
-                            break;
-                    }
-                    break;
-                case BluetoothConstants.MESSAGE_WRITE:
-                    byte[] writeBuf = (byte[]) msg.obj;
-                    // construct a string from the buffer
-                    String writeMessage = new String(writeBuf);
-                    break;
-                case BluetoothConstants.MESSAGE_READ:
-                    byte[] readBuf = (byte[]) msg.obj;
-                    // construct a string from the valid bytes in the buffer
-                    String readMessage = new String(readBuf, 0, msg.arg1);
-                    break;
-                case BluetoothConstants.MESSAGE_DEVICE_NAME:
-                    // save the connected device's name
-                    TextView header = (TextView) findViewById(R.id.textView);
-                    header.setText("Connected to " + mConnectedDeviceName);
-                    break;
-                case BluetoothConstants.MESSAGE_TOAST:
-                    // IDK what to do with this yet, so Log:
-                    Log.d(TAG, msg.getData().getString(BluetoothConstants.TOAST));
-                    break;
-            }
-        }
-    };
 
     public void sendMessage(View view) {
         // do something on button press
